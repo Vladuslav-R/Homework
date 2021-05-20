99eda0e1db5fcc9016d2b4e28a84661163ada5f9
Vladidlav <r_vlad@list.ru>
2021-05-20 22:34:44 +0300

    Task1

---
+++ src/Task4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int a = enterint("Введите число 1");
+        int b = enterint("Введите число 2");
+        System.out.println((a + b) + a * b);
+
+    }
+
+    public static int enterint(String message) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(message);
+        while (!scan.hasNextInt()) {
+            System.out.println(message);
+            scan.next();
+        }
+        return scan.nextInt();
+    }
+
+}
---
+++ src/Task6.java
@@ -0,0 +1,7 @@
+public class Task6 {
+    public static void main(String[] args) {
+        System.out.println("Введите число 2");
+
+    }
+}
+
