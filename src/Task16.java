dcb30d0b2ccaba5819d8a25d92a8d7066d84b1fc
Vladidlav <r_vlad@list.ru>
2021-05-23 21:11:19 +0300

    Task 15,16

---
+++ src/Task16.java
@@ -0,0 +1,11 @@
+public class Task16 {
+    public static void main(String[] agrs) {
+
+        int i;
+        for (i = 1; i < 100; i++) {
+            if (i%7 == 0) {
+                System.out.println(i + " "+ "Hope!");
+            }
+        }
+    }
+}
