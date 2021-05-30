1ce2ceae992a1996eec815b0af7b1f3cad5742d3
Vladidlav <r_vlad@list.ru>
2021-05-30 14:28:33 +0300

    Homework 31.05.2021

--- src/TaskTwentyTwoSecondEdition.java
+++ src/TaskTwentyTwoSecondEdition.java
@@ -1,15 +1,16 @@
-public class TaskTwentyTwo {
+public class TaskTwentyTwoSecondEdition {
     public static void main(String[] agrs) {
-        int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90,100};
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]+ " ");
 
         }
         System.out.println(" ");
 
-        for (int i = 0; i < array.length / 2; i++) {
+        for (int i = 0; i < array.length; i = i + 2) {
+            if (i == array.length - 1) break;
             int left = i;
-            int right = array.length - i-1;
+            int right =i + 1;
             swop(left, right, array);
         }
 
