283b67b88e763622871d4f0df8127cd4319dd9e2
Vladidlav <r_vlad@list.ru>
2021-06-06 20:49:10 +0300

    Homework 31.05.2021

---
+++ src/TaskTwentySeven.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class TaskTwentySeven {
+    public static void main(String[] args) {
+       String s = enterString("Введите текст");
+       s.trim();
+        char symbol;
+        String commas;
+        String spaces;
+
+        int numberOfSpaces = 0;
+        if(s.length() != 0) {
+            numberOfSpaces++;
+        }
+
+
+        for (int j = 0; j < s.length(); j++) {
+            symbol = s.charAt(j);
+            if (symbol == ' ') {
+                numberOfSpaces++;
+            }
+        }
+        if (s.charAt(0) == ' ') {
+            numberOfSpaces--;
+        }
+        if (s.charAt(s.length()-1) == ' ') {
+            numberOfSpaces--;
+        }
+
+        if (numberOfSpaces == 1) {
+            spaces = "слово";
+
+        }else if (numberOfSpaces % 10 == 1) {
+            spaces = "слов";
+        }  else if (numberOfSpaces % 10 == 2 || numberOfSpaces % 10 == 3||numberOfSpaces % 10 == 4) {
+            spaces = "слова";
+        }else if(numberOfSpaces>10||numberOfSpaces<20) {
+            spaces = "слов";
+        } else {
+            spaces = "слов";
+        }
+
+
+
+
+
+
+        System.out.println( numberOfSpaces+ " "+spaces);
+
+
+        }
+
+
+    public static String enterString(String message) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(message);
+        return scan.nextLine();
+    }
+}
