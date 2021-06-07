55885c66f13a7c84e697864447d39a0b6d3570c0
Vladidlav <r_vlad@list.ru>
2021-06-07 07:01:02 +0300

    Homework 07.06.2021

--- src/University.java
+++ src/University.java
@@ -11,25 +11,22 @@ public class University {
 
         student[0] = new Human("Appolon", "Hitrozadov",2000, 1, "Zoofac");
         student[1] = new Human("Traktorina", "Chumazova", 2000, 1, "Mehfac");
-        student[2] = new Human("Algebrina", "Botanova", 2000,1,"Zauckasfac");
+        student[2] = new Human("Algebrina", "Botanova", 1988,1,"Zauckasfac");
         student[3] = new Human("Vladimir", "Ulyanov", 1870,13, "Revolucionno_predatelskiy");
 
 
-        //System.out.println(student[0].birthday.toString());
-        System.out.println(student[0].getFaculty() );
-    }
-}
 
 
 
-        /*for (int i = 0; i < student.length; i++  ) {
-            if (student[i].id > 1235)
-                System.out.println(student[i].name);
+
+
+        for (int i = 0; i < student.length; i++  ) {
+            if (student[i].getYearOfBirth() <= 1999)
+                System.out.println(student[i].getYearOfBirth());
         }
 
-        //System.out.println(student[0].name);
+
     }
 
 
     }
-*/
