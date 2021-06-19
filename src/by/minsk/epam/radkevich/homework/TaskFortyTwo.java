8582797b2cad3f197b1bb7a168a5fd55d0a851d7
Vladidlav <r_vlad@list.ru>
2021-06-19 17:18:01 +0300

    Homework 19.06.2021

---
+++ src/by/minsk/epam/radkevich/homework/TaskFortyTwo.java
@@ -0,0 +1,34 @@
+package by.minsk.epam.radkevich.homework;
+
+import java.util.*;
+
+public class TaskFortyTwo {
+    public static void main(String[] args) {
+        String message = enterString("enter a sentence");
+        String search = enterString("enter key");
+
+        String[] words = message.toLowerCase().replaceAll("[-.?!)(,:]", "").split("\\s");
+
+        Map<String, Integer> counterMap = new TreeMap<>();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                Integer count = counterMap.get(word);
+                if (count == null) {
+                    count = 0;
+                }
+                counterMap.put(word, ++count);
+            }
+        }
+
+        System.out.println(search + ": " + (counterMap.containsKey(search) ? counterMap.get(search) : 0));
+
+
+    }
+    public static String enterString(String message) {
+        System.out.println(message);
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+
+
+}
