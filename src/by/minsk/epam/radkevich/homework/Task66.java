2e106ef5eafe53e9b722eab6f89f8b703e21af24
Vladidlav <r_vlad@list.ru>
2021-07-13 21:05:17 +0300

    Homework 14.07.2021

---
+++ src/by/minsk/epam/radkevich/homework/Task66.java
@@ -0,0 +1,12 @@
+package by.minsk.epam.radkevich.homework;
+
+import java.time.LocalDate;
+
+public class Task66 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+
+        System.out.println(today.minusDays(60));
+
+    }
+}
