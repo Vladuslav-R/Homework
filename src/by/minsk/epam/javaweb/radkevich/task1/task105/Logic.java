98c00c8e01bae23f35c57617a9fba66be1b4d868
Vladislav Radkevich <r_vlad@list.ru>
2021-10-10 21:22:05 +0300

    lesson 2 task

--- src/by/minsk/epam/javaweb/radkevich/task1/task105/Logic.java
+++ src/by/minsk/epam/javaweb/radkevich/task1/task105/Logic.java
@@ -1,5 +1,23 @@
 package by.minsk.epam.javaweb.radkevich.task1.task105;
 
 public class Logic {
+    public static boolean isPerfect(int number) {
+        int sum = 0;
+        int i = 1;
 
+        while (i < number) {
+            if (number % i == 0) {
+                sum += i;
+                i++;
+            } else {
+                i++;
+            }
+        }
+        if (sum == number) {
+            System.out.println(number);
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/by/minsk/epam/javaweb/radkevich/task1/task105/Task5.java
+++ src/by/minsk/epam/javaweb/radkevich/task1/task105/Task5.java
@@ -1,7 +1,6 @@
 package by.minsk.epam.javaweb.radkevich.task1.task105;
 
-import java.util.Scanner;
-
+import static by.minsk.epam.javaweb.radkevich.task1.task105.Logic.isPerfect;
 import static by.minsk.epam.radkevich.utils.EnterValue.enterInt;
 
 public class Task5 {
@@ -10,31 +9,5 @@ public class Task5 {
     public static void main(String[] args) {
         int b = enterInt("Введите число");
         System.out.println(isPerfect(b));
-    }
-
-
-    public static boolean isPerfect(int b) {
-        for (int number = 1; number <= b; number++) {
-            int sum = 0;
-            int i = 1;
-
-            while (i < number) {
-
-                if (number % i == 0) {
-                    sum += i;
-                    i++;
-                } else {
-                    i++;
-                }
-            }
-            if (sum == number) {
-                System.out.println(number);
-            }
-
-            }
-
-
-
-        return false;
     }
 }
