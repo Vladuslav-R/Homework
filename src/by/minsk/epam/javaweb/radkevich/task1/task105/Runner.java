063ba27b4f9c6cb251bef177c4f02c97d6f4e712
Vladislav Radkevich <r_vlad@list.ru>
2021-10-10 21:25:05 +0300

    lesson 2 task

--- src/by/minsk/epam/javaweb/radkevich/task1/task105/Runner.java
+++ src/by/minsk/epam/javaweb/radkevich/task1/task105/Runner.java
@@ -1,9 +1,8 @@
 package by.minsk.epam.javaweb.radkevich.task1.task105;
-
 import static by.minsk.epam.javaweb.radkevich.task1.task105.Logic.isPerfect;
 import static by.minsk.epam.radkevich.utils.EnterValue.enterInt;
 
-public class Task5 {
+public class Runner {
     // 5.	Составить программу, печатающую значение true, если указанное высказывание является истинным,
     // и false — в противном случае: является ли целое число совершенным (сумма делителей равна самому числу).
     public static void main(String[] args) {
--- src/by/minsk/epam/javaweb/radkevich/task1/task107/Task7.java
+++ src/by/minsk/epam/javaweb/radkevich/task1/task107/Task7.java
@@ -1,10 +1,9 @@
 package by.minsk.epam.javaweb.radkevich.task1.task107;
-
-import java.util.Scanner;
+import static by.minsk.epam.radkevich.utils.EnterValue.enterDouble;
 
 public class Task7 {
-   // 7.	Даны две точки А(х1, у1) и В(х2, у2). Составить
-   // алгоритм, определяющий, которая из точек находится ближе к началу координат. x y.
+    // 7.	Даны две точки А(х1, у1) и В(х2, у2). Составить
+    // алгоритм, определяющий, которая из точек находится ближе к началу координат. x y.
 
     public static void main(String[] agrs) {
         double ax = enterDouble("Enter the value of X at point A");
@@ -12,29 +11,14 @@ public class Task7 {
         double bx = enterDouble("Enter the value of X at point B");
         double by = enterDouble("Enter the value of Y at point B");
 
-
-        double pointAdistanceСalculation =  Math.abs(Math.pow(ax,2)+Math.pow(ay,2));
-        double pointBdistanceСalculation =  Math.abs(Math.pow(bx,2)+Math.pow(by,2));
-        if (pointAdistanceСalculation>pointBdistanceСalculation) {
-            System.out.println( "point A is closer to the origin xy");
-        } else if (pointBdistanceСalculation>pointAdistanceСalculation) {
-            System.out.println( "point B is closer to the origin xy");
+        double pointAdistanceСalculation = Math.abs(Math.pow(ax, 2) + Math.pow(ay, 2));
+        double pointBdistanceСalculation = Math.abs(Math.pow(bx, 2) + Math.pow(by, 2));
+        if (pointAdistanceСalculation > pointBdistanceСalculation) {
+            System.out.println("point A is closer to the origin xy");
+        } else if (pointBdistanceСalculation > pointAdistanceСalculation) {
+            System.out.println("point B is closer to the origin xy");
+        } else {
+            System.out.println("point A and point B is equidistant from xy origin");
         }
-        else {
-            System.out.println("point A and point B is equidistant from xy origin");}
-
-    }
-
-    public static double enterDouble(String message) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println(message);
-
-        while (!scan.hasNextDouble()) {
-            System.out.println(message);
-            scan.next();
-        }
-        return scan.nextDouble();
-
-
     }
 }
